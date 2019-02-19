4d1a06ae5bfab63b6b29de6c4b9aafe155f873a4
Jason Briggs <jason.briggs001@gmail.com>
2019-02-19 15:50:21 +0000

    Added file lock for all update and delete methods on website

--- app_client/video-all-versions/video-all-versions.controller.js
+++ app_client/video-all-versions/video-all-versions.controller.js
@@ -2,7 +2,7 @@ angular
     .module('aafapp')
     .controller('videoAllVersionsCtrl', videoAllVersionsCtrl);
 
-    function videoAllVersionsCtrl ($routeParams, videoData, $location, authentication) {
+    function videoAllVersionsCtrl ($routeParams, videoData, $location, authentication, $route) {
         var videoID = $routeParams.videoid;
 
         var vm = this;
@@ -12,12 +12,42 @@ angular
         if (!vm.isLoggedIn) {
             $location.path('/login');
         } else {
+            
+            vm.fileLock = "";
+            vm.username = authentication.currentUser().email;
+
             videoData.getAllVersions(videoID).then(function (result) {
                 vm.videos = result.data;
                 vm.videoDisplayDate = Date(vm.videos.videoData.videoCreationDate)
+                
+                vm.fileLock = vm.videos.fileisLocked;
+                vm.fileLockUser = vm.videos.fileLockedBy;
+
                 console.log(vm.videos);
             });
     
+            vm.lockFile = function () {
+                var lockData = {
+                    fileLock: true,
+                    fileLockedBy: vm.username
+                };
+
+                videoData.lockFile(videoID, lockData).then(function (result) {
+                    $route.reload();
+                });
+            };
+
+            vm.unlockFile = function () {                
+                var lockData = {
+                    fileLock: 'false',
+                    fileLockedBy: ''
+                };
+
+                videoData.lockFile(videoID, lockData).then(function (result) {
+                    $route.reload();
+                });
+            };
+
             vm.deleteVideo = function () {
                 console.log('video will be deleted');
                 videoData.deleteVideo(videoID).then(function () {
--- app_client/video-details/video-details.controller.js
+++ app_client/video-details/video-details.controller.js
@@ -2,7 +2,7 @@ angular
     .module('aafapp')
     .controller('videoDetailsViewCtrl', videoDetailsViewCtrl);
 
-    function videoDetailsViewCtrl ($routeParams, videoData, $location, authentication) {
+    function videoDetailsViewCtrl ($routeParams, videoData, $location, authentication, $route) {
         var videoID = $routeParams.videoid;
 
         var vm = this;
@@ -13,13 +13,42 @@ angular
             $location.path('/login');
         } else {
 
+            vm.fileLock = "";
+            vm.username = authentication.currentUser().email;
+
             videoData.getVideoDetails(videoID).then(function (result) {
                 vm.video = result.data;
                 vm.videoID = videoID;
+
+                vm.fileLock = vm.video.fileisLocked;
+                vm.fileLockUser = vm.video.fileLockedBy;
+
                 vm.videoVersion = vm.video.videoData[0].versionID;
                 vm.videoDisplayDate = Date(vm.video.videoData[0].videoCreationDate);
                 console.log(vm.video);
             });
+            
+            vm.lockFile = function () {
+                var lockData = {
+                    fileLock: true,
+                    fileLockedBy: vm.username
+                };
+
+                videoData.lockFile(videoID, lockData).then(function (result) {
+                    $route.reload();
+                });
+            };
+
+            vm.unlockFile = function () {                
+                var lockData = {
+                    fileLock: 'false',
+                    fileLockedBy: ''
+                };
+
+                videoData.lockFile(videoID, lockData).then(function (result) {
+                    $route.reload();
+                });
+            };
 
             vm.updateVersion = function () {
                 console.log('update clicked');
